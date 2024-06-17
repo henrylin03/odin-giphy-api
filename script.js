a14797279d93fd3f7f2bcb3fdf9e9d17d0613346
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-17 15:44:12 +1000

    clarify which button is being used to generate new gif in script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const API_KEY = "KNd6jJdzmPNQOSSRPCzCZPZnnxpSda56";
 const img = document.querySelector("img");
-const btn = document.querySelector(".new-gif");
+const newGifBtn = document.querySelector(".new-gif");
 
 getGIF();
-btn.addEventListener("mousedown", getGIF);
+newGifBtn.addEventListener("mousedown", getGIF);
 
 function getGIF() {
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=dogs`, {
