fa4c9b2d60c0e844fc6cd99161cec7c2c30da9b1
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-17 17:23:58 +1000

    apply await to the actual fetching

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ async function getGIF() {
   errorMessage.textContent = "";
   gifContainer.replaceChildren();
 
-  fetch(
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search}`,
     {
       mode: "cors",
     }
-  )
-    .then((response) => response.json())
+  );
+
+  response
+    .json()
     .then((response) => {
       const responseData = response.data;
 
