420f93428bf8f0dade0bd2435685d77c87602585
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-17 15:55:51 +1000

    updating the searchbar updates the search value and thus the fetch

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const API_KEY = "KNd6jJdzmPNQOSSRPCzCZPZnnxpSda56";
 const img = document.querySelector("img");
 const newGifBtn = document.querySelector(".new-gif");
+const searchbar = document.querySelector("#searchbar");
+const searchBtn = document.querySelector("#search-btn");
 
 getGIF();
 newGifBtn.addEventListener("mousedown", getGIF);
+searchBtn.addEventListener("mousedown", getGIF);
 
+// functions
 function getGIF() {
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=dogs`, {
-    mode: "cors",
-  })
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchbar.value}`,
+    {
+      mode: "cors",
+    }
+  )
     .then((response) => response.json())
     .then((response) => (img.src = response.data.images.original.url));
 }
-
-// event listener on the search button such that when it's mousedowned, the value of the searchbar is used to be added to the end of the url for fetching
-
-// when new gif is clicked again, then the search term is kept and we go again
